4da638750a3fdc9e54499c68886dfbad0baf6188
RayFaxiu <133870822+RayFaxiu@users.noreply.github.com>
2023-05-26 22:35:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,8 +4,7 @@ local lplr = Players.LocalPlayer
 local yes = Players.LocalPlayer.Name
 local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Whitelist.lua"))()
 local ChatTag = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/ChatTags.lua"))()
-local command =
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Commands.lua", true))()
+local commands = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Commands.lua", true))()
 
 local playerlist = game:GetService("CoreGui"):FindFirstChild("PlayerList")
 local players = game:GetService("Players")
