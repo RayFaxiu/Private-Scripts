c29607f66e152d4c7e6e40bb8d9c494f44e9498b
RayFaxiu <133870822+RayFaxiu@users.noreply.github.com>
2023-05-26 22:35:59 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,9 +2,9 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local lplr = Players.LocalPlayer
 local yes = Players.LocalPlayer.Name
-local whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Whitelist.lua"))()
+local Whitelist = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Whitelist.lua"))()
 local ChatTag = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/ChatTags.lua"))()
-local commands = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Commands.lua", true))()
+local Commands = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Commands.lua", true))()
 
 local playerlist = game:GetService("CoreGui"):FindFirstChild("PlayerList")
 local players = game:GetService("Players")
