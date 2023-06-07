7c9efb54980932a2b759a699de744b054b506d78
RayHafz <133870822+RayFaxiu@users.noreply.github.com>
2023-06-07 15:33:35 +0700

    Update HWID Loader.lua

--- HWID Loader.lua
+++ HWID Loader.lua
@@ -1,1 +1,9 @@
-
+local HWIDTable = loadstring(game:HttpGet("https://raw.githubusercontent.com/RayFaxiu/Private-Scripts/main/Whitelist2.lua"))()
+local HWID = game:GetService("RbxAnalyticsService"):GetClientId()
+for i,v in pairs(HWIDTable) do
+    if v == HWID then
+        print("Whitelisted!")
+        wait "1"
+        print("Loading Commands!")
+    end
+end
